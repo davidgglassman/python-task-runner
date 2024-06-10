d6df2ec6f2283fd3a615e2253d4a3d91b360c2ec
David Glassman <davidg@controlconcepts.net>
2024-06-10 13:48:01 -0400

    Implement automation to build single .exe

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+REM Create virtual environment
+python -m venv env
+
+REM Install packages
+pip install -r requirements.txt
+
+REM Activate virtual environment
+call env\Scripts\activate.bat
+
+REM Install packages
+pip install -r requirements.txt
+
+REM Run pyinstaller within the same window
+pyinstaller app.spec --distpath .
+
+REM Deactivate virtual environment
+deactivate
